402efc522a305bc2039bcd61f6d0f40608fcaa63
Daniel Bui <dnlbui@proton.me>
2023-01-04 14:40:04 -0600

    added json data, access array in object, parse id

--- routes/beers.js
+++ routes/beers.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-const beers = [
-  { id: '1', name: 'IPA' },
-  { id: '2', name: 'Stout' },
-  { id: '3', name: 'Brown' },
-  { id: '4', name: 'Pilsner' }
-];
+const beers = {
+  "beers": [
+    {
+      "id": 1,
+      "name": "512 IPA",
+      "reviews": [{ "id": 1, "text": "delicious" }]
+    },
+    {
+      "id": 2,
+      "name": "Durty Bull Brett",
+      "reviews": [
+        { "id": 2, "text": "why is Brett in the name?" },
+        { "id": 3, "text": "Brett, or brat? Yuck!" },
+        { "id": 4, "text": "Mmmm this is so good." },
+        { "id": 5, "text": "Q Dogs!" },
+        { "id": 6, "text": "^ Megan, is that you!?" }
+      ]
+    },
+    { "id": 3, "name": "Dogfish Head 90 Minute IPA", "reviews": [] },
+    {
+      "id": 4,
+      "name": "Chocolate stout",
+      "reviews": [
+        { "id": 7, "text": "too much Chocolate" },
+        { "id": 8, "text": "so yummy" },
+        { "id": 9, "text": "the best!" }
+      ]
+    }
+  ]
+}
 
 router.param('beer', function(req, res, next, id) {
-  req.beer = beers.find(beer => beer.id === id);
-
+  req.beer = beers.beers.find(beer => beer.id === parseInt(id));
   next();
 });
 
